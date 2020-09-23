e8d6eb2a76af4c5452daab7a3b3335e931fe211e
AlI230 <39678985+AlI230@users.noreply.github.com>
2020-09-23 12:17:56 +0200

    Update init-db-solution.sql

--- db/init-db-solution.sql
+++ db/init-db-solution.sql
@@ -1,5 +1,5 @@
 CREATE TABLE locations (
-	location_id INT NOT NULL,
+	location_id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (location_id),
 	name VARCHAR(45) NOT NULL,
 	adress VARCHAR(45) NOT NULL,
@@ -8,17 +8,17 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE groups (
-	group_id INT,
-    PRIMARY KEY (group_id),
+	group_id INT AUTO_INCREMENT,
+    	PRIMARY KEY (group_id),
 	name VARCHAR(45) NOT NULL,
 	story LONGTEXT,
 	logo VARCHAR(50) NOT NULL,
-    location_id INT NOT NULL,
+    	location_id INT NOT NULL,
 	CONSTRAINT `location_id` FOREIGN KEY (`location_id`) REFERENCES locations(location_id)
 );
 
 CREATE TABLE users (
-	user_id INT NOT NULL,
+	user_id INT NOT NULL AUTO_INCREMENT,
 	firstname VARCHAR(45) NOT NULL,
 	lastname VARCHAR(45) NOT NULL,
 	email VARCHAR(50) NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE activities (
-	activity_id INT NOT NULL,
+	activity_id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (activity_id),
 	start_date DATETIME NOT NULL,
 	end_date DATETIME NOT NULL,
@@ -40,6 +40,6 @@ CREATE TABLE activities (
 	description VARCHAR(45) NOT NULL,
 	start_publication DATE,
 	end_publication DATE,
-    group_id INT NOT NULL,
+    	group_id INT NOT NULL,
 	CONSTRAINT `group_id` FOREIGN KEY (`group_id`) REFERENCES groups(group_id)
 );
