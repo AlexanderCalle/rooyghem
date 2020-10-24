8f0cb4f510ca9b5e5b461ceeb5ebfd9fe6e1d60d
Michiel Rogissart <michiel.rogissart@gmail.com>
2020-10-24 13:32:13 +0200

    added some dummy data to sql file

--- db/fill-db.sql
+++ db/fill-db.sql
@@ -11,4 +11,12 @@ VALUES("Kabouters", "Kabouters hebben vk", "path/kabouter/logo", 1),
       ("Jonghernieuwers", "Jonghernieuwers hebben vk", "path/jonghernieuwers/logo", 3),
       ("Aspiranten", "Aspiranten hebben vk", "path/aspiranten/logo", 2),
       ("Hernieuwers", "Hernieuwers hebben vk", "path/hernieuwers/logo", 2),
-      ("Oudercomite", "OC hebben vk", "path/oc/logo", 2)	  
+      ("Oudercomite", "OC hebben vk", "path/oc/logo", 2)
+
+INSERT INTO activities (activity_id, title, start_date, end_date, meeting_point, description, start_publication, end_publication, group_id)
+VALUES (1, "KAB 1", "2020-10-24T14:00:00", "2020-10-24T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe kabouteractiviteit", "2020-10-20", "2020-11-10", 1),
+       (2, "KAB 2", "2020-10-31T14:00:00", "2020-10-31T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe kabouteractiviteit 2", "2020-10-20", "2020-11-10", 1),
+       (3, "KAB 3", "2020-11-07T14:00:00", "2020-11-07T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe kabouteractiviteit 3", "2020-10-20", "2020-11-10", 1),
+       (4, "PAG 1", "2020-10-24T14:00:00", "2020-10-24T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe pagadderactiviteit", "2020-10-20", "2020-11-10", 2),
+       (5, "PAG 2", "2020-10-31T14:00:00", "2020-10-31T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe pagadderactiviteit 2", "2020-10-20", "2020-11-10", 2),
+       (6, "PAG 3", "2020-11-07T14:00:00", "2020-11-07T17:00:00", "Engelendalelaan 18, 8310 Sint-Kruis","Een super toffe pagadderactiviteit 3", "2020-10-20", "2020-11-10", 2)
