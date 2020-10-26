eeba4ca2497881daca5d82fbdfa8e729e809271d
Michiel Rogissart <michiel.rogissart@gmail.com>
2020-10-26 13:08:42 +0100

    updating of neswfeed works

--- router/newsfeeds.js
+++ router/newsfeeds.js
@@ -61,75 +61,46 @@ router.post('/create', authCheck,(req, res)=>{
 
 // TODO: page for updating a newsfeed
 // Router GET update newsfeed
-// router.get('/update/:id', authCheck,(req, res)=>{
-//     con.query('SELECT * from activities WHERE activity_id = ?', req.params.id, (err, activity)=>{
-//         if(err) return res.render('badrequest', {error: err});
-//         con.query('SELECT name FROM `groups` WHERE group_id = ?', activity[0].group_id, (err, group_name)=>{
-//             if(err) return res.render('badrequest', {error: err});
-//             res.render('update_activity', {
-//                 activity: activity[0],
-//                 group_name: group_name[0].name,
-//                 group_id: activity[0].group_id,
-//                 admin: req.admin,
-//                 moment: require('moment')
-//             })
-//         });
-//     });
-// });
+router.get('/update/:id', authCheck,(req, res)=>{
+    con.query('SELECT * from newsfeeds WHERE feed_id = ?', req.params.id, (err, newsfeed)=>{
+        if(err) return res.render('badrequest', {error: err});
+        
+        res.render('update_newsfeed', {
+            newsfeed: newsfeed[0],
+            admin: req.admin,
+            moment: require('moment')
+        });
+    });
+});
 
 // TODO: update one newsfeed
 // Route UPDATE One activity
-// router.put('/update/:id', authCheck,(req, res)=>{
-//     const data = req.body;
-//     const updated_activity = {
-//         title: data.title,
-//         start_date: data.start_date,
-//         end_date: data.end_date,
-//         meetingpoint: data.meetingpoint,
-//         description: data.description,
-//         start_publication: data.start_publication,
-//         end_publication: data.end_publication,
-//         group_id: data.group_id
-//     }
-//     if(updated_activity != null) {
-//         if(req.user.user_id == 1) {
-//             con.query(`UPDATE activities SET ? WHERE activity_id = ?`, [updated_activity, req.params.id], (err, activity)=>{
-//                 if(err) return res.render('badrequest', {error: err});
-//                 res.redirect('/activities');
-//             });
-//         } else if(data.group_id === req.user.group_id.toString()) {
-//             con.query(`UPDATE activities SET ? WHERE activity_id = ?`, [updated_activity, req.params.id], (err, activity)=>{
-//                 if(err) return res.render('badrequest', {error: err});
-//                 res.redirect('/activities');
-//             });
-//         } else {
-//             con.query('SELECT * from activities WHERE activity_id = ?', req.params.id, (err, activity)=>{
-//                 if(err) return res.render('badrequest', {error: err});
-//                 con.query('SELECT name FROM `groups` WHERE group_id = ?', activity[0].group_id, (err, group_name)=>{
-//                     if(err) return res.render('badrequest', {error: err});
-//                     res.render('update_activity', {
-//                         activity: activity[0],
-//                         group_name: group_name[0].name,
-//                         group_id: activity[0].group_id,
-//                         error: 'cannot update activity from another group'
-//                     });
-//                 });
-//             });
-//         }
-//     } else {
-//         con.query('SELECT * from activities WHERE activity_id = ?', req.params.id, (err, activity)=>{
-//             if(err) return res.render('badrequest', {error: err});
-//             con.query('SELECT name FROM `groups` WHERE group_id = ?', activity[0].group_id, (err, group_name)=>{
-//                 if(err) return res.render('badrequest', {error: err});
-//                 res.render('update_activity', {
-//                     activity: activity[0],
-//                     group_name: group_name[0].name,
-//                     group_id: activity[0].group_id,
-//                     error: 'Please fill all fields in'
-//                 });
-//             });
-//         });
-//     }
-// });
+router.put('/update/:id', authCheck,(req, res)=>{
+    const updated_newsfeed = {
+        title: req.body.title,
+        description: req.body.description,
+        start_publication: req.body.start_publication,
+        end_publication: req.body.end_publication,
+        picture_path: req.body.picture_path,
+        created_by: req.user.user_id
+    }
+    console.log(updated_newsfeed);
+    if(updated_newsfeed != null) {
+        con.query(`UPDATE newsfeeds SET ? WHERE feed_id = ?`, [updated_newsfeed, req.params.id], (err, newsfeed)=>{
+            if(err) return res.render('badrequest', {error: err});
+            res.redirect('/newsfeed');
+        });
+    } else {
+        con.query('SELECT * from newsfeeds WHERE feed_id = ?', req.params.id, (err, newsfeed)=>{
+            if(err) return res.render('badrequest', {error: err});
+            
+            res.render('update_newsfeed', {
+                newsfeed: newsfeed[0],
+                admin: req.admin,
+                moment: require('moment')
+            });
+        });
+    }
+});
 
 module.exports = router;
