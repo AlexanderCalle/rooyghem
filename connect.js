0acfffd0a2663cfd068360648bfc3020c19e0d39
Alexander Callebaut <alexander@MBPvanAlexander.lan>
2020-11-10 14:24:08 +0100

    sociale media icons and backoffice nav

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ const con = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'R00yghem,8310',
+    password: process.env.MYSQL_PASSWORD || 'root',
     database: 'rooyghem'
 });
 
