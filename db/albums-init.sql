b739fee747a70da15f91bd93f9d091db236c34ba
Alexander Callebaut <alexander@MBPvanAlexander.lan>
2020-12-03 17:53:40 +0100

    album init script

---
+++ db/albums-init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE albums (
+    album_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    group_id INT NOT NULL,
+    description LONGTEXT,
+    activity_start DATE,
+    activity_end DATE,
+    creation_date DATE,
+    PRIMARY KEY(album_id),
+    FOREIGN KEY (group_id) REFERENCES `groups`(group_id)
+)
+
+CREATE TABLE pictures (
+    pictures_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    album_id INT,
+    path VARCHAR(255),
+    upload_date DATE,
+    was_approved BOOLEAN DEFAULT FALSE,
+    approved_by VARCHAR(255) NOT NULL,
+    approved_on DATE,
+    PRIMARY KEY(pictures_id),
+    FOREIGN KEY (album_id) REFERENCES `albums`(album_id),
+    CONSTRAINT `approved_by` FOREIGN KEY (approved_by) REFERENCES users(user_id)
+)
