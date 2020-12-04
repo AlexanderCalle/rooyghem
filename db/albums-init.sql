6607eba0e3fd2c7fd9818e202cc067cafb3a9eae
Alexander Callebaut <alexander@MBPvanAlexander.lan>
2020-12-04 11:54:46 +0100

    minor changes

--- db/albums-init.sql
+++ db/albums-init.sql
@@ -7,9 +7,11 @@ CREATE TABLE albums (
     activity_end DATE,
     creation_date DATE,
     checked BOOLEAN DEFAULT FALSE,
+    approved_by VARCHAR(255) NOT NULL,
+    approved_on DATE,
     PRIMARY KEY(album_id),
     FOREIGN KEY (group_id) REFERENCES `groups`(group_id)
-)
+);
 
 CREATE TABLE pictures (
     pictures_id INT NOT NULL AUTO_INCREMENT,
@@ -17,10 +19,6 @@ CREATE TABLE pictures (
     album_id INT,
     path VARCHAR(255),
     upload_date DATE,
-    was_approved BOOLEAN DEFAULT FALSE,
-    approved_by VARCHAR(255) NOT NULL,
-    approved_on DATE,
     PRIMARY KEY(pictures_id),
-    FOREIGN KEY (album_id) REFERENCES `albums`(album_id),
-    CONSTRAINT `approved_by` FOREIGN KEY (approved_by) REFERENCES users(user_id)
-)
+    FOREIGN KEY (album_id) REFERENCES `albums`(album_id)
+);
