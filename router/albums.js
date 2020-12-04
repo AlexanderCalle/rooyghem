5f8542ab0e07bda617f87d2f7bec9538e6464f04
Alexander Callebaut <alexander@MBPvanAlexander.lan>
2020-12-04 16:12:03 +0100

    interface albums ...

--- router/albums.js
+++ router/albums.js
@@ -14,11 +14,22 @@ router.get('/' , (req, res)=> {
     });
 });
 
+router.get('/album/:id', (req, res) => {
+    con.query('SELECT * FROM albums WHERE album_id = ?', req.params.id, (err, album) => {
+        if(err) return res.render('badrequest', {error: err});
+        res.render('album_photos', {
+            user: req.user,
+            admin: req.admin,
+            album: album[0],
+        });
+    });
+});
+
 router.get('/create', (req, res)=> {
     res.render('album_create', {
         user: req.user,
         admin: req.admin,
-    })
+    });
 });
 
 router.post('/create', (req, res)=> {
@@ -45,15 +56,54 @@ router.post('/create', (req, res)=> {
             user: req.user,
             admin: req.admin,
             error: 'Vul alles in!'
-        })
+        });
     }
-})
+});
+
+router.get('/update/:id', (req, res)=> {
+    con.query('SELECT * FROM albums WHERE album_id = ?', req.params.id, (err, album) => {
+        if(err) return res.render('badrequest', {error: err});
+        res.render('album_update', {
+            user: req.user,
+            admin: req.admin,
+            album: album[0],
+            moment: require('moment')
+        });
+    });
+});
+
+router.put('/update/:id', (req, res)=> {
+    data = req.body;
+    album_data = {
+        name: data.name,
+        group_id: req.user.group_id,
+        description: data.description,
+        activity_start: data.activity_start,
+        activity_end: data.activity_end,
+    }
+
+    con.query('UPDATE albums SET ? WHERE album_id = ?', [album_data ,req.params.id], (err, album)=> {
+        if (err) return res.render('badrequest', {error: err});
+        res.redirect('/albums')
+    });
+});
+
+router.get('/delete/:id', (req, res)=> {
+    con.query('DELETE FROM albums WHERE album_id = ?', req.params.id, (err, album)=>{
+        if(err) return res.render('badrequest', {error: err});
+        res.redirect('/albums');
+    });
+});
 
 router.get('/checker', (req, res)=> {
-    res.render('album_check', {
-        admin: req.admin,
-        user: req.user,
-    })
+    con.query('SELECT * FROM albums WHERE checked = 0', (err, albums)=>{
+        if(err) return res.render('checker', {error: err});
+        res.render('album_check', {
+            admin: req.admin,
+            user: req.user,
+            albums: albums
+        });
+    });
 });
 
 module.exports = router;
