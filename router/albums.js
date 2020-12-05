897bab5364e0b7f1d097f1c83de33c9340e329fb
Alexander Callebaut <alexander@MBPvanAlexander.lan>
2020-12-05 12:26:11 +0100

    adding pic upload

--- router/albums.js
+++ router/albums.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const moment = require('moment');
 const con = require('../connect');
+const multer = require('multer');
+const fs = require('fs');
+const compression = require('../middleware/compression');
 const router = express.Router();
+
+// Multer middleware Save images
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, process.env.TEMP_PATH)
+    },
+    filename: (req, file, cb) =>{
+        cb(null, Date.now() + file.originalname.replace(/\s/g, ''));
+        console.log(file);
+    }
+});
+
+
+const upload = multer({ storage: storage });
 
 router.get('/' , (req, res)=> {
     con.query('SELECT * FROM albums WHERE group_id = ?', req.user.group_id, (err, albums)=> {
@@ -16,12 +33,16 @@ router.get('/' , (req, res)=> {
 
 router.get('/album/:id', (req, res) => {
     con.query('SELECT * FROM albums WHERE album_id = ?', req.params.id, (err, album) => {
-        if(err) return res.render('badrequest', {error: err});
-        res.render('album_photos', {
-            user: req.user,
-            admin: req.admin,
-            album: album[0],
-        });
+        if (err) return res.render('badrequest', {error: err});
+        con.query('SELECT * FROM pictures WHERE album_id = ?', req.params.id, (err, pictures) => {
+            if(err) return res.render('badrequest', {error: err});
+            res.render('album_photos', {
+                user: req.user,
+                admin: req.admin,
+                album: album[0],
+                pictures: pictures
+            });
+        })
     });
 });
 
@@ -46,10 +67,13 @@ router.post('/create', (req, res)=> {
             activity_end: data.activity_end,
             creation_date: moment(creation_date).format('YYYY-MM-DD HH:mm:ss'),
         }
-        console.log(album_data);
-        con.query('INSERT INTO `albums` SET ?', album_data, (err, album)=> {
+        con.query('INSERT INTO `albums` SET ?', album_data, (err, result)=> {
             if(err) return res.render('badrequest', {error: err});
-            res.send('ok')
+            var dir = process.env.ALBUMS_PATH+ '/' + data.name + result.insertId + '/'
+            if(!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+                res.redirect('/albums/album/' + result.insertId);
+            }
         });
     } else {
         res.render('album_create', {
@@ -92,6 +116,52 @@ router.get('/delete/:id', (req, res)=> {
     con.query('DELETE FROM albums WHERE album_id = ?', req.params.id, (err, album)=>{
         if(err) return res.render('badrequest', {error: err});
         res.redirect('/albums');
+    });
+});
+
+router.get('/album/:id/add', (req, res) => {
+    con.query('SELECT * FROM albums WHERE album_id = ?', req.params.id, (err, album)=> {
+        if (err) return res.render('badrequest', {error: err});
+        res.render('pic_add', {
+            user: req.user,
+            admin: req.admin,
+            album: album[0]
+        });
+    });
+});
+router.post('/album/:id/add',upload.array('pic') , (req, res) => {
+    con.query('SELECT * FROM albums WHERE album_id = ?', req.params.id, async (err, album)  => {
+        if (err) return res.render('badrequest', {error: err});
+        
+        await req.files.forEach(function(file) {
+            const pic_destination = process.env.ALBUMS_PATH + '/' + album[0].name + album[0].album_id + '/'
+            const pic_destination_site = process.env.ALBUMS_PATH_SITE + '/' + album[0].name + album[0].album_id + '/'
+            compression( process.env.TEMP_PATH + file.filename, pic_destination);
+            const pic_data = {
+                name: file.filename,
+                album_id: album[0].album_id,
+                path: pic_destination_site + file.filename,
+                upload_date: moment(Date.now()).format('YYYY-MM-DD')
+            }
+
+            con.query('INSERT INTO pictures SET ?', pic_data, (err, pic_data)=> {
+                if(err) return res.render('badrequest', {error: err});
+                console.log('succes');
+            })
+        });
+
+        res.redirect('/albums/album/' + album[0].album_id)
+    });
+});
+
+router.get('/album/:album_id/pic/delete/:pictures_id', (req, res)=> {
+    con.query('SELECT * FROM pictures WHERE pictures_id = ?', req.params.pictures_id, (err, pic)=> {
+        if (err) return res.render('badrequest', {error: err});
+        fs.unlinkSync('.' + pic[0].path);
+        con.query('DELETE FROM pictures WHERE pictures_id = ?', req.params.pictures_id, (err, pic)=>{
+            if(err) return res.render('badrequest', {error: err});
+            res.redirect('/albums/album/' + req.params.album_id);
+        });
     });
 });
 
