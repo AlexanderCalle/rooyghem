460babc0d4f0f343eb39e9ab5a36eba4a1df2994
Ali230 <callebauta@hotmail.com>
2021-10-03 16:35:25 +0200

    ophaal moment wafelbak

--- db/orders-update.sql
+++ db/orders-update.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `rooyghem_dev`.`orders` 
 CHANGE COLUMN `email` `phone` VARCHAR(255) NOT NULL ;
+
+ALTER TABLE `Rooyghem`.`orders` 
+ADD COLUMN `pick_up_moment` VARCHAR(255) NOT NULL AFTER `order_date`;
