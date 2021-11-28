b13dc66eed445455779bc00517da14bfda81303b
Ali230 <callebauta@hotmail.com>
2021-11-28 12:03:14 +0100

    merge errors 2

--- backend/router/groups.js
+++ backend/router/groups.js
@@ -5,24 +5,16 @@ const path = require('path');
 
 router.get('/', (req, res) => {
     con.query('SELECT * FROM `groups` WHERE group_id <= 6', (err, groups) => {
-<<<<<<< HEAD:backend/router/groups.js
         if (err) return res.status(400).json({ "statuscode": 400, error: err });
         groups.forEach(group => {
             group.logo = '/groups/' + group.name + '/logo';
         })
         return res.status(200).json({
             groups: groups
-=======
-        if (err) return res.render('badrequest', { error: err });
-        res.render('groups', {
-            groups: groups,
-            username: req.user.username
->>>>>>> develop:router/groups.js
         });
     })
 });
 
-<<<<<<< HEAD:backend/router/groups.js
 router.get('/:group_name/logo', (req, res) => {
     con.query('SELECT logo FROM `groups` WHERE name = ?', req.params.group_name, (err, logo_path) => {
         if (err) return res.status(400).json({ "statuscode": 400, error: err });
@@ -114,29 +106,6 @@ router.get('/:group_name/info', (req, res) => {
                                 moment: require('moment')
                             });
                         }
-=======
-router.get('/:group_name/info', (req, res) => {
-    con.query('SELECT * FROM `groups` WHERE name = ?', req.params.group_name, (err, group) => {
-
-        if (err) return res.render('badrequest', { error: err });
-
-        if (group.length === 0) return res.render('badrequest', { error: "No group found for " + req.params.group_name })
-
-        con.query('SELECT * FROM locations WHERE location_id = ?', group[0].location_id, (err, location) => {
-            if (err) return res.render('badrequest', { error: err });
-
-            con.query('SELECT * FROM activities WHERE group_id = ? AND end_publication > ?', [group[0].group_id, new Date()], (err, activities) => {
-                if (err) return res.render('badrequest', { error: err });
-                con.query('SELECT firstname, lastname, email, is_banleader, path_pic FROM users WHERE group_id = ? ORDER BY is_banleader DESC, lastname ASC', group[0].group_id, (err, leaders) => {
-                    if (err) return res.render('badrequest', { error: err });
-                    res.render('./group_pages/info', {
-                        group: group[0],
-                        location: location[0],
-                        leaders: leaders,
-                        activities: activities,
-                        username: req.user.username,
-                        moment: require('moment')
->>>>>>> develop:router/groups.js
                     });
                 })
             });
@@ -144,20 +113,12 @@ router.get('/:group_name/info', (req, res) => {
     });
 });
 
-<<<<<<< HEAD:backend/router/groups.js
 router.get('/:group_name/info/activities', (req, res) => {
     con.query('SELECT * FROM `groups` WHERE name = ?', req.params.group_name, (err, groups) => {
         if (err) return res.status(400).json({ "statuscode": 400, error: err });
         if (groups[0] == null) return res.status(404).json({ "statuscode": 404, error: "Group not found" });
         con.query('SELECT * FROM activities WHERE group_id = ?', groups[0].group_id, (err, activities) => {
             if (err) return res.status(400).json({ "statuscode": 400, error: err });
-=======
-router.get('/:group_name/info/events', (req, res) => {
-    con.query('SELECT * FROM `groups` WHERE name = ?', req.params.group_name, (err, groups) => {
-        if (err) return res.render('badrequest', { error: err });
-        con.query('SELECT * FROM activities WHERE group_id = ?', groups[0].group_id, (err, activities) => {
-            if (err) return res.render('badrequest', { error: err });
->>>>>>> develop:router/groups.js
             let events = [];
             activities.forEach(activity => {
                 events.push({
