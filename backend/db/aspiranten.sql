1f0e1b92f44ad9be94831ef13952e0c6bd59c322
Michiel Rogissart <michiel.rogissart@gmail.com>
2021-12-07 15:18:04 +0100

    edited sql aspiranten

--- backend/db/aspiranten.sql
+++ backend/db/aspiranten.sql
@@ -1,5 +1,5 @@
 CREATE TABLE aspiranten (
-    aspi_id INT NOT NULL AUTO_INCREMENT,
+    aspi_id VARCHAR(256) NOT NULL,
     firstname VARCHAR(45) NOT NULL,
     lastname VARCHAR(45) NOT NULL,
     path_pic VARCHAR(256),
