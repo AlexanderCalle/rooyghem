3f54894a3b930ab7716fd360647e59015d5a08e0
Ali230 <callebauta@hotmail.com>
2022-01-16 14:16:57 +0100

    css tokshop/design

--- frontend/src/pages/TokshopOrderPage.js
+++ frontend/src/pages/TokshopOrderPage.js
@@ -24,19 +24,18 @@ function TokshopOrderPage() {
             const itemsJson = await itemsRes.json();
             const groupsJson = await groupsRes.json();
             var tempOrder = new Map();
-            
+
             itemsJson.items.forEach(item => {
                 tempOrder.set(item, 0);
             })
             setOrder(tempOrder);
             setGroups(groupsJson.groups);
         }
-
         fetchData();
     }, [setOrder, setGroups]);
 
     if (!order || !groups) {
-        return(
+        return (
             <>
                 <Navbar />
                 <main class="container" id="tokshopcontainer">
@@ -51,16 +50,18 @@ function TokshopOrderPage() {
         var tempMap = new Map(order);
         const startAmount = tempMap.get(itemInfo);
         var factor = 1;
-        if(!doIncrement) {
+        if (!doIncrement) {
             factor = -1;
         }
-        const newvalue = startAmount + (1*factor);
+        const newvalue = startAmount + (1 * factor);
         if (newvalue >= 0) {
             tempMap.set(itemInfo, newvalue);
             setOrder(tempMap);
             setTotalPrice(totalPrice + (itemInfo.price * factor));
+            console.log(tempMap);
+            console.log(tempMap.get(itemInfo))
         }
-        
+
     };
 
     const postOrder = async () => {
@@ -69,8 +70,8 @@ function TokshopOrderPage() {
         formData.append("firstname", firstname);
         formData.append("lastname", lastname);
         formData.append("group_id", groupid);
-        
-        var items = Array.from(order).filter(([k, v]) => {return v > 0;}).map(([item, amount])=>({tokshopitem_id: item.tokshopitem_id, amount: amount}));
+
+        var items = Array.from(order).filter(([k, v]) => { return v > 0; }).map(([item, amount]) => ({ tokshopitem_id: item.tokshopitem_id, amount: amount }));
         formData.append("items", items);
         const requestOptions = {
             method: 'POST',
@@ -86,7 +87,7 @@ function TokshopOrderPage() {
 
         fetch(`${process.env.REACT_APP_BACKEND_HOST}/tokshop/orders`, requestOptions)
             .then(response => {
-                response.json().then(json => {})
+                response.json().then(json => { })
             });
 
         // fetch(`https://jsonplaceholder.typicode.com/`, requestOptions)
@@ -98,96 +99,110 @@ function TokshopOrderPage() {
     const makeItemList = () => {
         return (
             <>
-                <div id="tokshoplist">
-                {
-                    Array.from(order).map(([itemInfo, amount]) => (
-                        <div class="tokshopitem">
-                            <div class="imgdiv tokshopitemimg">
-                                <img src={itemInfo.picture}/>
-                            </div>
-                            <div class="tokshopiteminfo">
-                                <h3>{itemInfo.name}</h3>
-                                <p>{itemInfo.description}</p>
-                            </div>
-                            <h4>€{itemInfo.price}</h4>
-                            <div class="tokshopitemcontrol">
-                                <button onClick={() => changeAmount(itemInfo, false)}>-</button>
-                                <p><b>{amount}</b></p>
-                                <button onClick={() => changeAmount(itemInfo, true)}>+</button>
-                            </div>
-                        </div>
-                    ))
-                }
-                </div>
                 <div id="tokshopcontrol">
-                <p><b>Totale prijs: €{totalPrice.toFixed(2)}</b></p>
-                <button onClick={e => setShowItemsList(false)}>Bestelling afronden</button>
+                    <h2>Tokshop</h2>
+                    <div className="tokshopcontroltotal">
+                        <p><b>Totale prijs: €{totalPrice.toFixed(2)}</b></p>
+                        <button onClick={e => setShowItemsList(false)}>Bestellen</button>
+                    </div>
                 </div>
-        </>
+                <div id="tokshoplist">
+                    {
+                        Array.from(order).map(([itemInfo, amount]) => (
+                            <div class="tokshopitem">
+                                <div class="imgdiv b-line">
+                                    <img src={itemInfo.picture} />
+                                </div>
+                                <div class="tokshopiteminfo">
+                                    <h3>{itemInfo.name}</h3>
+                                    <h4>€{itemInfo.price}</h4>
+                                    {/* <p>{itemInfo.description}</p> */}
+                                </div>
+                                <div class="tokshopitemcontrol">
+                                    {order.get(itemInfo) > 0 ? (
+                                        <div>
+                                            <button className="addminbtn" onClick={() => changeAmount(itemInfo, false)}>-</button>
+                                            <p><b>{amount}</b></p>
+                                            <button className="addminbtn" onClick={() => changeAmount(itemInfo, true)}>+</button>
+                                        </div>
+                                    ) : (
+                                        <button onClick={() => changeAmount(itemInfo, true)} className="tokshopitembtn">Voeg toe!</button>
+                                    )}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+
+            </>
         );
     };
 
     const makeUserForm = () => {
-        return(
-        <div id="finishorderview">
-            <div id="orderlist">
-                <div id="orderlistitems">
-                {
-                    Array.from(order)
-                    .filter(([k, v]) => {return v > 0;})
-                    .map(([item, amount]) => (
-                        <div class="orderitem">
-                            <div class="imgdiv">
-                                <img src={item.picture} />
-                            </div>
-                            <p>{item.name}</p>
-                            <p>{amount}</p>
-                            <p>€{item.price.toFixed(2)}</p>
-                        </div>
-                    ))
-                }
+        return (
+            <div id="finishorderview">
+                <div id="orderlist">
+                    <div id="orderlistitems">
+                        {
+                            Array.from(order)
+                                .filter(([k, v]) => { return v > 0; })
+                                .map(([item, amount]) => (
+                                    <div class="orderitem">
+                                        <div class="imgdiv">
+                                            <img src={item.picture} />
+                                        </div>
+                                        <div className="orderiteminfo">
+                                            <p><b>{item.name}</b></p>
+                                            <div className="orderiteminfoprice">
+                                                <p>Aantal: {amount}</p>
+                                                <p>Prijs: €{item.price.toFixed(2)}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                        }
+                    </div>
+                    <div id="ordertotalpricediv">
+                        <p><b>Totaal: €{totalPrice.toFixed(2)}</b></p>
+                    </div>
                 </div>
-                <div id="ordertotalpricediv">
-                    <p><b>Totaal: €{totalPrice.toFixed(2)}</b></p>
-                </div>
-            </div>
-            <form onSubmit={postOrder}>
-                <label for="email">Emailadres</label>
-                <input type="email" value={email} placeholder="Emailadres" name="email" onChange={(e) => setEmail(e.target.value)} required />
-                <label>Voornaam lid <i>(bestelling zal meegegeven worden op activiteit)</i></label>
-                <input type="text" value={firstname} placeholder="Voornaam lid" name="firstname" onChange={(e) => setFirstname(e.target.value)} required/>
-                <label>Achternaam lid <i>(bestelling zal meegegeven worden op activiteit)</i></label>
-                <input type="text" value={lastname} placeholder="Achternaam lid" name="lastname" onChange={(e) => setLastname(e.target.value)} required/>
-                <label>Ban waartoe het lid behoort</label>
-                
-                <select id="bannen" name="group" value={groupid ? groupid : 1} onChange={e => setGroupid(e.target.value)}>
-                    {groups.map((group) => (
-                        <option value={group.group_id}>{group.name}</option>
-                    ))
-                    }
-                </select>
-                <div>
-                    <button onClick={e => setShowItemsList(true)}>Bestelling aanpassen</button>
-                    <button type="submit"
-                    disabled={totalPrice <= 0}>
-                                            Bestelling plaatsen</button>
-                    <ReCAPTCHA
-                    ref={recaptchaRef}
-                    sitekey="6Lda4E4cAAAAAEMITzaJKTyhPGHZuDuKK4kFuCOD"
-                    size="invisible"
-                    />
-                </div>
-            </form>
-        </div>)
+                <form onSubmit={postOrder}>
+                    <label for="email">Emailadres</label>
+                    <input type="email" value={email} placeholder="Emailadres" name="email" onChange={(e) => setEmail(e.target.value)} required />
+                    <label>Voornaam lid <i>(bestelling zal meegegeven worden op activiteit)</i></label>
+                    <input type="text" value={firstname} placeholder="Voornaam lid" name="firstname" onChange={(e) => setFirstname(e.target.value)} required />
+                    <label>Achternaam lid <i>(bestelling zal meegegeven worden op activiteit)</i></label>
+                    <input type="text" value={lastname} placeholder="Achternaam lid" name="lastname" onChange={(e) => setLastname(e.target.value)} required />
+                    <label>Ban waartoe het lid behoort</label>
+
+                    <select id="bannen" name="group" value={groupid ? groupid : 1} onChange={e => setGroupid(e.target.value)}>
+                        {groups.map((group) => (
+                            <option value={group.group_id}>{group.name}</option>
+                        ))
+                        }
+                    </select>
+                    <div>
+                        <button onClick={e => setShowItemsList(true)}>Bestelling aanpassen</button>
+                        <button type="submit"
+                            disabled={totalPrice <= 0}>
+                            Bestelling plaatsen</button>
+                        <ReCAPTCHA
+                            ref={recaptchaRef}
+                            sitekey="6Lda4E4cAAAAAEMITzaJKTyhPGHZuDuKK4kFuCOD"
+                            size="invisible"
+                        />
+                    </div>
+                </form>
+            </div>)
     };
 
-    return(
+    return (
         <>
-        <Navbar/>
-        <main class="container" id="tokshopcontainer">
-            {showItemsList ? makeItemList() : makeUserForm()}
-        </main>
-        <Footer/>
+            <Navbar />
+            <main class="container" id="tokshopcontainer">
+                {showItemsList ? makeItemList() : makeUserForm()}
+            </main>
+            <Footer />
         </>
     );
 }
